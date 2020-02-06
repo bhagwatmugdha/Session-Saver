30fe5e541e2da94989e63a2f58f0bce64fb31d12
mugdhabhagwat9 <mugdhabhagwat9@gmail.com>
2020-02-07 00:34:51 +0530

    Added options page with all sessions and delete button

--- options.js
+++ options.js
@@ -1,5 +1,80 @@
 document.getElementById("delete_all_sessions").onclick = function () {
-    chrome.storage.sync.clear(function(result){
-        
+    chrome.storage.local.clear(function (result) {
     })
+    window.location.reload();
+}
+
+window.onload = function load_table() {
+    var tab_table_div = document.getElementById("tab_table");
+
+    chrome.storage.local.get('session_tabs', function (result) {
+        if (result.session_tabs) {
+
+            chrome.storage.local.get('session_names', function (session_name_result) {
+                var tab_names = result.session_tabs;
+
+                for (tab in tab_names) {
+                    var session_name = tab_names[tab];
+
+                    var session_tabs = session_name_result.session_names[session_name];
+
+                    // Div to wrap around session title and delete button
+                    var wrap_div = document.createElement("div");
+
+                    //  Create a h3 for the session name
+                    var session_name_title = document.createElement("h3");
+                    session_name_title.style = "margin: 10px; display: inline-block;";
+                    session_name_title.innerHTML = session_name;
+
+                    //  Create a button to delete the session
+                    var deleteButton = document.createElement("button");
+                    deleteButton.className = "btn btn-danger";
+                    deleteButton.style = "margin: 4px; float:right";
+                    deleteButton.innerHTML = "Delete " + session_name;
+                    deleteButton.type = "submit"
+
+                    // Funtion on click to delete the session
+                    deleteButton.onclick = function () {
+                        var session_name = this.innerHTML.split("Delete ")[1];
+
+                        chrome.storage.local.get('session_tabs', function (result) {
+                            var tab_names = result.session_tabs;
+                            tab_names.splice(tab_names.indexOf(session_name), 1);    chrome.storage.local.set({session_tabs: tab_names});
+                        })
+
+                        chrome.storage.local.get('session_names', function (result) {
+                            var session_names = result.session_names;
+                            delete session_names[session_name];
+                            chrome.storage.local.set({session_names});
+                        })
+                        window.location.reload();
+                    };
+
+                    wrap_div.appendChild(session_name_title);
+                    wrap_div.appendChild(deleteButton);
+
+                    //  Create a table
+                    var table = document.createElement("table");
+                    table.className = "table table-bordered table-striped";
+                    table.style = "padding: 10px;"
+
+                    for (tab in session_tabs) {
+                        var tr = document.createElement('tr');
+                        tr.style = "padding: 10px;";
+
+                        var td = document.createElement('td');
+                        td.style = "padding: 10px";
+                        td.innerHTML = '<a href=' + session_tabs[tab].url + '>' + session_tabs[tab].title + '</a>';
+
+                        tr.appendChild(td);
+                        table.appendChild(tr);
+                    }
+
+                    tab_table_div.appendChild(wrap_div);
+                    tab_table_div.appendChild(table);
+                }
+            })
+        }
+    })
+
 }
--- session_saver_popup.js
+++ session_saver_popup.js
@@ -2,33 +2,34 @@ document.getElementById("save_session").onclick = function () {
     var session_name = document.getElementById("session_name").value;
 
     if (session_name) {
-        // Fetch all tabs in the current window
-        chrome.tabs.query({ currentWindow: true }, function (tabs) {
-            
-            chrome.storage.sync.get('session_names', function(result){
-                if(result.session_names){
-                    session_names = result.session_names;
-                    session_names[session_name] = tabs;
-                    chrome.storage.sync.set({session_names})
-                }
-                else {
-                    var session_names = {}
-                    session_names[session_name] = tabs;
-                    chrome.storage.sync.set({session_names})
-                }
-            })
-
-        })
-
-        chrome.storage.sync.get('session_tabs', function (result) {
+        chrome.storage.local.get('session_tabs', function (result) {
             if (result.session_tabs) {
                 var tab_names = result.session_tabs;
+                if(tab_names.includes(session_name)){
+                    alert("Session name already exists, please give another name!");
+                    return;
+                }   
             }
             else {
                 var tab_names = []
             }
             tab_names.push(session_name);
-            chrome.storage.sync.set({ session_tabs: tab_names })
+            chrome.storage.local.set({ session_tabs: tab_names })
+        })
+
+        // Fetch all tabs in the current window
+        chrome.tabs.query({ currentWindow: true }, function (tabs) {
+            
+            chrome.storage.local.get('session_names', function(result){
+                if(result.session_names){
+                    session_names = result.session_names;
+                }
+                else {
+                    var session_names = {}
+                }
+                session_names[session_name] = tabs;
+                chrome.storage.local.set({session_names})
+            })
         })
     }
     else {
@@ -37,9 +38,8 @@ document.getElementById("save_session").onclick = function () {
 }
 
 window.onload = function load_sessions() {
-    chrome.storage.sync.get(null, function callback(items) { console.log(items) });
-    
-    chrome.storage.sync.get('session_tabs', function (result) {
+
+    chrome.storage.local.get('session_tabs', function (result) {
         if (result.session_tabs) {
             var tab_names = result.session_tabs;
             for (tab in tab_names) {
@@ -58,14 +58,11 @@ window.onload = function load_sessions() {
                 newButton.onclick = function () {
                     var session_name = this.innerHTML;
                     
-                    chrome.storage.sync.get('session_names', function (result) {
-                        // alert("clicked", Object.keys(result))
+                    chrome.storage.local.get('session_names', function (result) {
 
                         var session_tabs = result.session_names[session_name];
-                        // alert(session_tabs[session_name])
                         
                         for (tab in session_tabs) {
-                            // alert(session_tabs[tab].url)
                             chrome.tabs.create({ url: session_tabs[tab].url }, function () {
                             })
                         }
